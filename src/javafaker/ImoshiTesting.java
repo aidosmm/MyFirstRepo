e0ea61cda4f21752b88b3c953b9b8624224481f6
Aidos Mukashev <aidos.mukashev.m@gmail.com>
2022-06-22 18:38:28 +0600

    Conflict

--- src/javafaker/ImoshiTesting.java
+++ src/javafaker/ImoshiTesting.java
@@ -1,4 +1,4 @@
-package main;
+package javafaker;
 
 public class ImoshiTesting {
     public static void main(String[] args) {
--- src/javafaker/Test123.java
+++ src/javafaker/Test123.java
@@ -1,4 +1,4 @@
-package main;
+package javafaker;
 
 public class Test123 {
     public static void main(String[] args) {
